86b8cf958e68be4401516846da4530d3d69a3807
lochuynh <lochuynh12@gmail.com>
2020-01-10 13:54:24 +0700

    fix index.js

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ var nodeStatic = require("node-static");
 var http = require("http");
 var socketIO = require("socket.io");
 
-// var fileServer = new nodeStatic.Server();
+var fileServer = new nodeStatic.Server();
 var app = http
   .createServer(function(req, res) {
     fileServer.serve(req, res);
   })
   .listen(8080);
-
-var app = http.createServer().listen(8080);
 
 var io = socketIO.listen(app);
 
